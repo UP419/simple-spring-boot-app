31f22c2ee15fd0226a5c76a255542eb7807ef5a3
ucha <uchaucha419@gmail.com>
2024-01-17 22:37:01 +0400

    added simple rest get call

--- src/main/java/com/example/simplespringbootapplication/SimpleSpringBootApplication.java
+++ src/main/java/com/example/simplespringbootapplication/SimpleSpringBootApplication.java
@@ -2,12 +2,32 @@ package com.example.simplespringbootapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
-public class SimpleSpringBootApplication {
+@RestController
+class HelloController {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SimpleSpringBootApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HelloController.class, args);
+    }
+
+    @GetMapping(value = "/{number}")
+    public ResponseEntity<?> hello(@PathVariable int number) {
+        String result = "none";
+        if (number % 15 == 0) {
+            result = "fizzbuzz";
+        } else if (number % 5 == 0) {
+            result = "fizz";
+        } else if (number % 3 == 0) {
+            result = "buzz";
+        }
+        return new ResponseEntity<>("Result is , " + result, HttpStatus.OK);
+    }
 
 }
