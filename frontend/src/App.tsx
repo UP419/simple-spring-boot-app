be73cee16c41694469440f81b826f091eaaa80fb
ucha <uchaucha419@gmail.com>
2024-01-18 11:37:33 +0400

    added output text

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,30 @@
-import React, { useState, FormEvent } from 'react';
+import React, {FormEvent, useState} from 'react';
 
-interface AppProps {}
+interface AppProps {
+}
 
 const App: React.FC<AppProps> = () => {
-    // State to manage the value of the input box
     const [inputValue, setInputValue] = useState<string>('');
+    const [outputValue, setOutputValue] = useState<string>('');
 
     const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
+        if (isNaN(Number(inputValue))) {
+            setOutputValue("The input is not a valid number");
+        } else {
+            try {
+                const response = await fetch(`http://localhost:8080/${inputValue}`);
 
-        try {
-            const response = await fetch(`http://localhost:8080/15`);
-
-            if (response.ok) {
-                const res = await response.text();
-                console.log('Request successful, result is ' + res);
-            } else {
-                console.error('Request failed');
+                if (response.ok) {
+                    const res = await response.text();
+                    setOutputValue(res);
+                    console.log('Request successful, result is ' + res);
+                } else {
+                    console.error('Request failed');
+                }
+            } catch (error) {
+                console.error('Error occurred:', error);
             }
-        } catch (error) {
-            console.error('Error occurred:', error);
         }
 
         setInputValue('');
@@ -27,6 +32,7 @@ const App: React.FC<AppProps> = () => {
 
     return (
         <div>
+            <h1>{outputValue}</h1>
             <form onSubmit={handleSubmit}>
                 <label>
                     Input:
